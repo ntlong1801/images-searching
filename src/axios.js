9efee5913c027a7cfb73c740a9c1c1d1179e1437
ntlong1801 <ntlongcbl@gmail.com>
2023-10-29 14:12:19 +0700

    Updates

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'https://api.unsplash.com/photos',
-    headers: 'Authorization: Client-ID wRkEst0s11lqExCKj9mKjgLVDaKZLj39ERgKSwe6dHE',
-
-})
-
-export default instance;
